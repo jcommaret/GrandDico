07252af4ff0097831f12ab0ec2fc2d989754a08a
Jérôme Commaret <jcommaret@gmail.com>
2024-06-25 23:07:02 +0200

    android

---
+++ android/app/src/main/java/com/granddictionnaire/jcommaret/MainActivity.java
@@ -0,0 +1,5 @@
+package com.granddictionnaire.jcommaret;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.granddictionnaire.jcommaret',
+  appName: 'granddictionnaire',
+  webDir: 'build'
+};
+
+export default config;
